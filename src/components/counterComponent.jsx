81249fb090c753de2e572012e6f2f683292b92ca
refreshed-hub <tkrefreshed@gmail.com>
2021-03-08 03:01:49 +0400

    Counter has been committed.

---
+++ src/components/counterComponent.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.counter.value,
+  };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ value: this.state.value - 1 });
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <React.Fragment>
+        <h4>Counter #{this.props.counter.id}</h4>
+        <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-secondary btn-sm m-2"
+        >
+          Delete
+        </button>
+        <br></br>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatValue() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
